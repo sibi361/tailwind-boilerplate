edabf776c8dee304567a64347c3cec496560c48b
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-10-26 13:12:22 +0530

    CSS Not Building Yet

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-    root: "src",
-    content: ["./src/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    build: {
-        outDir: "../dist",
-    },
+    content: ["*"],
     server: { host: "127.0.0.1" },
     css: {
         postcss: {
